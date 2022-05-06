6be0b108934032e954cf1094baf3d7da48dc8db1
Tsiferaniaina <tsitsifidimamisoa@gmail.com>
2022-05-06 22:59:47 +0200

    NowayHome first versio

---
+++ scripts/adversaires.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+    $('.card-hero').on({
+        mouseenter: function() {
+            $(this).css({
+                "transform": "scale(1.05)",
+                "cursor": "pointer",
+                "transition": " all 0.5s ease-in-out",
+                "color": "var(--white-ad-color)",
+                "background-color": "var(--black-ad-color)"
+
+            })
+        },
+        mouseleave: function() {
+            $(this).css({
+                "transform": "scale(1)",
+                "cursor": "pointer",
+                "transition": " all 0.5s ease-in-out",
+                "color": "var(--black-ad-color)",
+                "background-color": "var(--primary-ad-color)"
+
+            })
+        }
+    })
+
+    
+    // social media hover
+    $('.mediahover').on({
+            mouseenter: function() {
+                $(this).css('color', 'white')
+            },
+            mouseleave: function() {
+                $(this).css('color', 'black')
+            }
+        })
+     
+
+    // header on mouse over
+
+    $('.rubriques').on({
+        mouseenter: function() {
+            $(this).css("text-decoration", "line-through")
+        },
+        mouseleave: function() {
+            $(this).css("text-decoration", "none")
+        }
+    })
+
+    
+    //titre animation
+
+    $('H1').animate({
+        left: '15px'
+    }, 5000)
+    $('.description').fadeIn(3500)
+
+
+
+});
---
+++ scripts/heros.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+
+    // header on mouse over
+
+    $('.rubriques').on({
+            mouseenter: function() {
+                $(this).css("text-decoration", "line-through")
+            },
+            mouseleave: function() {
+                $(this).css("text-decoration", "none")
+            }
+        })
+    
+        //titre animation
+
+    $('H1').animate({
+        left: '15px'
+    }, 5000)
+    $('.description').fadeIn(3500)
+    $('H3').fadeIn(3500)
+
+    
+    // social media hover
+    $('.mediahover').on({
+        mouseenter: function() {
+            $(this).css('color', 'white')
+        },
+        mouseleave: function() {
+            $(this).css('color', 'black')
+        }
+    })
+
+    $('.zoom').on({
+        mouseenter: function() {
+            $(this).css({
+                "transform": "scale(1.1)",
+                "transition": " all 0.5s ease-in-out"
+            })
+        },
+        mouseleave: function() {
+            $(this).css({
+                "transform": "scale(1)",
+                "transition": " all 0.5s ease-in-out"
+            })
+        }
+    })
+
+
+
+});
+
+// slide heros
+var i = 0;
+var images = ['1.png', '2.png', '3.png', '4.png', '5.png'];
+
+function slideimg() {
+    document.slider.src = './assets/img/heroslider' + images[i];
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+    setTimeout("slideimg()", 2000);
+}
+window.load = slideimg();
